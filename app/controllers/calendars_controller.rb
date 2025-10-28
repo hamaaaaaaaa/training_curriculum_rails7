0588c79fc00093d060a2718e5b017f7c85abefe9
hamaaaaaaaa <hamatoten@gmail.com>
2025-10-28 21:54:20 +0900

    Revert "Merge pull request #8 from hamaaaaaaaa/second_training"
    This reverts commit e2d350e603098baaee74368b18eb2ba15a8e9187, reversing
    changes made to f43f1fb9259b6948934e8b396ef557b9642a3aa2.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,46 +8,35 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry  # ← ここで処理が止まり、paramsなどを確認できる
     Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
-  # フォームで送られてくるパラメータの指定
   def plan_params
-    # フォームは model: @plan なので require(:plan) が正解
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
-  # 1週間分の日付・曜日・予定を取得するメソッド
   def getWeek
-    wdays = ['(日)', '(月)', '(火)', '(水)', '(木)', '(金)', '(土)']
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
+    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
+    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+
     @week_days = []
 
-    # 今日から6日後までの予定を取得
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
-      today = @todays_date + x
       today_plans = []
-
       plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == today
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      # 曜日を追加
-      days = {
-        month: today.month,
-        date: today.day,
-        wday: wdays[today.wday],  # ← 曜日を追加！
-        plans: today_plans
-      }
-
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
+
   end
 end
